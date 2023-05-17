9202d6d6718e8d0040349556e440454a3014d8b5
jeffreytran98 <37007320+jeffreytran98@users.noreply.github.com>
2023-05-17 15:53:30 -0700

    Time: 2117 ms (67.95%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+
+#Select which columns you want
 SELECT unique_id, name
+#From which Table you want
 FROM Employees
+#Left JOIN - Returns ALL rows on Left (Employees.id) that match with Right (EmployeeUNI.id) w/ Null where unapplicable
 LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id
