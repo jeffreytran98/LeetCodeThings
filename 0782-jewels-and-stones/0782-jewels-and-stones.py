b29bfe1b43010ffb74e2626ad4da019c4dfb4a31
jeffreytran98 <37007320+jeffreytran98@users.noreply.github.com>
2023-05-17 15:33:08 -0700

    Time: 52 ms (8.74%), Space: 16.3 MB (16.66%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,20 +1,23 @@
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
-        return len([letter for letter in S if letter in J])
+        hashMap = {}
+        result = 0
+        #Counts all your stones
+        for stone in S:
+            if stone not in hashMap:
+                hashMap[stone] = 1
+            else:
+                hashMap[stone] += 1
 
+        #Compare, see how many of your jewels are stones
+        for jewel in J:
+            #If one of my jewels is a stone
+            if jewel in hashMap:
+                result += hashMap[jewel]
+        return result
 
+        #This works but it slow, use a hashmap
+        #return len([letter for letter in S if letter in J])
 
-
-
-
-
-
-
-
-
-
-
-        #This works
-        #return len([i for i in S if i in J])
 
         
