58f6912dd62ad3d4c410290c1042d28f6a9012d1
jeffreytran98 <37007320+jeffreytran98@users.noreply.github.com>
2023-05-19 16:28:45 -0700

    Time: 532 ms (95.97%), Space: 34.6 MB (5.69%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -8,7 +8,9 @@ class Solution:
             if n not in someHash:
                 someHash[n] = 1
             else:
+                #If n is not in someHash, it implies there is more than 1 n
                 return True
-                
+
+        #if it finishes, then all elements are distinct
         return False
         
